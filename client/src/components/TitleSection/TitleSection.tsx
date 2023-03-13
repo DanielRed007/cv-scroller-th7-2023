4720f62a8b57b41b2de4773e2815e939cab6fff0
daniel.mesu <daniel.mesu@coderio.co>
2023-03-13 01:24:40 +0000

    even more improvements :)

--- client/src/components/TitleSection/TitleSection.tsx
+++ client/src/components/TitleSection/TitleSection.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import InboxIcon from '@mui/icons-material/Inbox';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import CodeIcon from '@mui/icons-material/Code';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 export const TitleSection = () => {
   return (
@@ -6,6 +15,51 @@ export const TitleSection = () => {
         <div className="title-top">
           <h1>Daniel</h1>
           <h1>Mesu</h1>
+        </div>
+
+        <div className="title-bottom-section">
+          
+          <div className="title-contact">
+            <Stack direction="column" spacing={1}>
+              <div className="contact-block">
+                <AlternateEmailIcon style={{color: "#cccccc"}} /> <h3>danielrojo1927@gmail.com</h3>
+              </div>
+
+              <div className="contact-block">
+                <PhoneIphoneIcon style={{color: "#cccccc"}} /> <h3>+11 2696 5611</h3>
+              </div>
+            </Stack>
+          </div>
+
+          <div className="title-icons">
+            <Stack direction="row" spacing={1}>
+              <Chip
+                avatar={<CodeIcon style={{color: "white"}} />}
+                label="Codewars"
+                variant="outlined"
+                clickable
+              />
+
+              <Chip
+                avatar={<LinkedInIcon style={{color: "white"}} />}
+                label="LinkedIn"
+                variant="outlined"
+                clickable
+              />
+
+              <Chip
+                avatar={<GitHubIcon style={{color: "white"}} />}
+                label="Github"
+                variant="outlined"
+                clickable
+              />
+            </Stack>
+          </div>
+
+          <div>
+            download CV
+          </div>
+
         </div>
     </div>
   )
---
+++ client/src/components/sections/Customers.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
+
+export const Customers = () => {
+  return (
+    <div className="section-container">
+      <div className="title-container">
+        <h1 className="section-main-title"> Customers</h1>
+        <Diversity2Icon className="about-icon"/>
+      </div>
+
+      <p className="section-text-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam nihil iure temporibus enim quo quasi saepe soluta, esse rerum architecto magni quidem, nulla porro alias distinctio at ipsum sint laudantium!</p>
+      <p className="section-text-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam nihil iure temporibus enim quo quasi saepe soluta, esse rerum architecto magni quidem, nulla porro alias distinctio at ipsum sint laudantium!</p>
+    </div>
+  )
+}
